29601f404a03ea75ef47825e5ffe6b2e2a0318a5
Miguel Menjivar <menjivarmiguel221@gmail.com>
2020-02-12 20:25:40 -0800

    major change to project, added projects route and added github page, still needs to fix the github to run and display information from repo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('file-system');
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const fs = require('file-system');
 
 // ROOT ROUTE
 router.get('/', function (req, res) {
@@ -20,6 +21,42 @@ router.get('/home/about', function(req, res){
 // API ROUTE
 router.get('/api', function (req, res) {
     res.render('api');
+});
+
+// GITHUB ROUTE
+// one user only
+router.get('/api/github', function(req, res){
+    try {
+        const options = {
+            uri: encodeURI(`https://api.github.com/users/mamenjivar/repos?per_page=5&sort=created:asc`),
+            method: 'GET',
+            headers: {
+                'user-agent': 'node.js',
+                // Authorization: `token ` + process.env.GITHUB_TOKEN
+                Authorization: `token ${process.env.GITHUB_TOKEN}`
+            }
+        };
+        
+        request(options, function(error, response, body){
+            if(error) {
+                console.error(error);
+            }
+            if (response.statusCode != 200){
+                return res.status(404).json({ msg: 'Github profile not found' });
+            }
+
+            var data = JSON.parse(body);
+
+            res.render('github', {
+                gitData: data 
+            });
+        });
+    } catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Error (github)');
+    }
+
+    // res.render('github');
 });
 
 // to display resume for people to see and download
